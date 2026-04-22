78e9484ae047da20263b98e7157cc03e855ddda5
unknown <abdeecode@gmail.com>
2026-04-22 13:35:23 +0700

    navbar draft 2 all items

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,11 +1,32 @@
+import { nav_items } from '@/constants/navigation'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div className='py-[18px] px-[32px] w-full flex justify-between items-center'>
       <div className='flex gap-2'>
-        <h5 className='font-bold text-[20px]'>Geefi Residence</h5>
+        <h5 className='font-bold text-[20px] text-primary'>Geefi Residence</h5>
       </div>
+
+      <ul className='flex gap-8'>
+        {nav_items.map((item) => (
+          <li key={item.id}>
+            <Link 
+              href={item.href} 
+              className='transition-colors duration-200 text-[14px] text-secondary hover:text-primary font-regular'
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <Link 
+      href={"#"}
+      className='py-2.5 px-6 font-whte text-semibold font-[14px] rounded-full bg-primary text-white hover:bg-primary/90 transition-colors duration-200'>
+        Hubungi Kami
+      </Link>
     </div>
   )
 }
---
+++ src/constants/navigation.js
@@ -0,0 +1,9 @@
+
+export const nav_items = [
+  { id: 1, label: 'Beranda', href: '/' },
+  { id: 2, label: 'Tentang', href: '/tentang' },
+  { id: 3, label: 'Unit', href: '/unit' },
+  { id: 4, label: 'Fasilitas', href: '/fasilitas' },
+  { id: 5, label: 'Simulasi KPR', href: '/simulasi-kpr' },
+  {id: 6, label: 'Lokasi', href: '/lokasi' },
+];
