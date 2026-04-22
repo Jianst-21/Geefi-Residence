5f9034144f893b1cc45a8c7c08985b53757534b1
unknown <abdeecode@gmail.com>
2026-04-22 13:14:42 +0700

    draft navbar

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='py-[18px] px-[32px] w-full flex justify-between items-center'>
+      <div className='flex gap-2'>
+        <h5 className='font-bold text-[20px]'>Geefi Residence</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
